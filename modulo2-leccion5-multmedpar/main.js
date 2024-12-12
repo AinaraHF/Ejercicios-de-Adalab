d12a91c83e5fb8b5629e71df16449bde397832e7
AinaraHF <ainarahf@gmail.com>
2024-12-12 19:54:32 +0100

    (2/3 done)

--- modulo2-leccion5-multmedpar/main.js
+++ modulo2-leccion5-multmedpar/main.js
@@ -1,5 +1,21 @@
-function sum(2, 4, 6) {
-    const return = 2 + 4 + 6;
+function mult(a, b) {
+    return a*b;
   }
-  const amount = sum (2, 4, 6);
-  console.log('Cantidad', )
+  mult(2,4);
+  console.log(mult(2,4));
+
+function med (c, d, e, f){
+  return (c+d+e+f)/4;
+}
+  med(3,6,9,12);
+  console.log(med(3,6,9,12));
+
+function bool(g){
+  if(g===even){
+    bool true;
+  }else{
+    bool false;
+  }
+}
+  bool(2);
+  console.log((bool(2)));
