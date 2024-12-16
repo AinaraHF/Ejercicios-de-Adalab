033d22cefc4bf9e90c68ab42c3276ae6043d56c1
AinaraHF <ainarahf@gmail.com>
2024-12-16 20:20:31 +0100

    exercise 1 and 2 done, exercise 1.2 pending

--- modulo2-leccion7-bioadalabers/main.js
+++ modulo2-leccion7-bioadalabers/main.js
@@ -1,20 +1,32 @@
 'use strict';
-
+//Ejer1
 const desc1= document.querySelector('.js-adalaber1');
 const desc2= document.querySelector('.js-adalaber2');
 
 const adalaber1 = {
     name: "Susana",
     age: 34,
-    job: "periodista",
-  };
+    job: "periodista",    
+};
 
-  const adalaber2 = {
+const adalaber2 = {
     name: "Rocío",
     age: 25,
     job: "actriz",
   };
 
   desc1.innerHTML = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.job}.`;
-
   desc2.innerHTML = `Mi nombre es ${adalaber2.name}, tengo ${adalaber2.age} y soy ${adalaber2.job}.`;
+  
+  //Ejer2
+  adalaber1.run = (distance) => `Estoy corriedo un maratón de ${distance} kilómetros.`;
+console.log(adalaber1.run("50"));
+
+
+//Ejer1.2
+adalaber1.showBio = () => `Mi nombre es adalaber1.name, tengo ${adalaber1.age} y soy ${adalaber1.job}.`;
+    console.log(adalaber1.showBio);
+
+/*const showBio = function (){`Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.job}.`;};
+console.log(adalaber1.showBio);*/
+
