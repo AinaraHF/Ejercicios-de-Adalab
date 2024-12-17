c31b2d40833f3b8488d877a5667b10ab124606bb
AinaraHF <ainarahf@gmail.com>
2024-12-17 16:55:26 +0100

    pearBasket exercise done

--- modulo2-leccion7-crearcestaperas/main.js
+++ modulo2-leccion7-crearcestaperas/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const pearBasket = {
-    max: 10,
-    min: 2,
-    current: 8,
-    initial: 3,
+    maxiValue: 10,
+    minValue: 3,
+    currentValue: 8,
+    initialValue: 3,
 }
 
-pearBasket.add = (a) => `8 + ${phrase}`;
-console.log(pearBasket(2));
+pearBasket.add = () => 
+    pearBasket.currentValue +=1;
+
+//console.log(pearBasket.add());
+
+pearBasket.take = () => 
+    pearBasket.currentValue -=1;
+
+//console.log(pearBasket.take());
+
+pearBasket.reset = () => 
+    pearBasket.currentValue;
+
+console.log(pearBasket.reset());
