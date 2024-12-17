0940d23253390a5ab488c8cf9850d3c37763297b
AinaraHF <ainarahf@gmail.com>
2024-12-17 16:21:59 +0100

    arrays exercise done

---
+++ modulo2-leccion8-peliculas/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const films = ["Jurassic Park", "Forrest Gump", "Jack"];
+films[3] = "Harry Potter";
+films[2] = "Love actually";
+
+function workWithMovies (){
+    console.log(films);
+}
+
+workWithMovies();
+
