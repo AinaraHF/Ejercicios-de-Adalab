102d8b42167986fffba80981417368d263e48c75
AinaraHF <ainarahf@gmail.com>
2024-12-20 10:11:34 +0100

    exercise done, doing bonuses

--- modulo2-ejerciciointermedio/js/main.js
+++ modulo2-ejerciciointermedio/js/main.js
@@ -3,6 +3,10 @@
 const button = document.querySelector('.js-play');
 const select = document.querySelector('.js-select');
 const result = document.querySelector('.js-result');
+const player = document.querySelector('.js-player');
+const pc = document.querySelector('.js-pc');
+const reset = document.querySelector('.js-reset');
+
 
 function getRandomNumber(max){
     return Math.ceil(Math.random()*max);
@@ -11,13 +15,44 @@ function getRandomNumber(max){
 function getRandomMove(){
     const numRandom = getRandomNumber(9);
     if(numRandom <= 3){
-        let moveRandom = 'rock';
+        return 'rock';
     }else if(numRandom >= 7){
-        let moveRandom = 'paper';
+        return 'paper';
     }else{
-        let moveRandom = 'scissors';
+        return 'scissors';
     }
 }
 
+function counter(){
+    if(result.innerHTML === '¡Has Ganado!'){
+        return player.innerHTML++;
+    }else if(result.innerHTML === '¡Has perdido!'){
+        return pc.innerHTML++;
+    }
+}
+
+function maxPoints(){
+    if(player.innerHTML || pc.innerHTML === 10){
+        return reset.classList.remove('hidden');
+    }/*else if(pc.innerHTML === 10){
+        return reset.classList.remove('hidden');
+    }*/
+}
+
+function handleClick(ev){
+    ev.preventDefault();
+    const moveRandom = getRandomMove();
+    if(moveRandom === select.value){
+        result.innerHTML = 'Empate';
+    } else if(moveRandom === 'rock' && select.value === 'scissors' || moveRandom === 'scissors' && select.value === 'paper' || moveRandom === 'paper' && select.value === 'rock'){
+        result.innerHTML = '¡Has Ganado!';
+    } else if(moveRandom === 'rock' && select.value === 'paper' || moveRandom === 'scissors' && select.value === 'rock' || moveRandom === 'paper' && select.value === 'scissors'){
+        result.innerHTML = '¡Has perdido!';
+}
+    counter();
+    maxPoints();
+}
+
+button.addEventListener('click', handleClick)
 
 
