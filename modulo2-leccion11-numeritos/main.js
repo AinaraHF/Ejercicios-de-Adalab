b195e42eeab968909ca2c2cc579fb5aaa0cce9c6
AinaraHF <ainarahf@gmail.com>
2025-01-06 18:39:45 +0100

    new exercise numeritos

---
+++ modulo2-leccion11-numeritos/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const numbers = [];
+
+/*function get100Numbers (){
+   for (let i = 0; i < 100; i++){
+   numbers = [i]};
+}
+
+console.log(numbers.push(get100Numbers))*/
+
+for (let i = 0; i < 100; i++){
+    function get100Numbers (){
+        return numbers.push
+    }
+}
+
+console.log(get100Numbers)
