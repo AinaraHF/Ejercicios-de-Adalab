0c910f9c16444bb16196f077b811323ad041aa1c
AinaraHF <ainarahf@gmail.com>
2025-01-06 18:59:47 +0100

    exercise Thenumbers

---
+++ modulo2-leccion11-thenumbers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function bestLostNumber (){
+    const evenNumbers = [];
+    for (const numbers of lostNumbers){
+        lostNumbers % 2;
+    }
+    evenNumbers.push()
+}
+bestLostNumber()
+
+console.log(bestLostNumber)
