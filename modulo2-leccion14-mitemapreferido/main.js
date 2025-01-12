78db177947c37d182928061157b116d6a1716483
AinaraHF <ainarahf@gmail.com>
2025-01-12 16:41:59 +0100

    2.14.2 Exercise 2 not finished

---
+++ modulo2-leccion14-mitemapreferido/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const lightTheme = document.querySelector('.js-lighttheme');
+const darkTheme = document.querySelector('.js-darktheme');
+
+function handleChangeLight(){
+    const lightValue = lightTheme.value;
+    if(lightValue){
+    lightTheme.classList.add('lighttheme');
+    }else{}
+    localStorage.setItem('tema', 'tema claro')
+}
+
+function handleChangeDark(){
+    const darkValue = darkTheme.value;
+    if(darkValue){
+        darkTheme.classList.add('darktheme')
+    }else{}
+    localStorage.setItem('tema', 'tema oscuro')
+}
+
+lightTheme.addEventListener('change', handleChangeLight);
+darkTheme.addEventListener('change', handleChangeDark);
+
