88313e6a858be4fee76dbe486435b2b619e186ae
AinaraHF <ainarahf@gmail.com>
2025-01-23 12:59:25 +0100

    counter exercise done

--- modulo3/leccion2-contador/src/components/App.jsx
+++ modulo3/leccion2-contador/src/components/App.jsx
@@ -2,23 +2,23 @@ import {useState} from 'react';
 
 function App() {
   
-  const [count, setCount] = useState('');
+  const [counter, setCounter] = useState(0);
 
   const handleIncrease = () => {
-     setCount(count++);
+    setCounter(counter+1);
   }
 
   const handleDecrease = () => {
-
+    setCounter(counter-1);
   }
 
   const handleReset = () => {
-
+    setCounter(0);
   }
   return (
     <>
     <h1>Contador</h1>
-    <p>Contador: <span>0{count}</span></p>
+    <p>Contador: {counter}</p>
     <button onClick={handleIncrease}>Incrementar contador</button>
     <button onClick={handleDecrease}>Reducir contador</button>
     <button onClick={handleReset}>Resetear contador</button>
