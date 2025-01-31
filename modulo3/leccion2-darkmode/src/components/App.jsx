22390c7e4c89e8e3bb1c3f4aab80b4af77abc9ce
AinaraHF <ainarahf@gmail.com>
2025-01-31 16:35:33 +0100

    darkmode exercise almost done

--- modulo3/leccion2-darkmode/src/components/App.jsx
+++ modulo3/leccion2-darkmode/src/components/App.jsx
@@ -4,15 +4,32 @@ import "../styles/App.css";
 function App() {
   
   const [mode, setMode] = useState("");
-  const handleClick = () => {
+  const [text, setText] = useState("");
 
+  const handleClick = (ev) => {
+    ev.preventDefault()
+    if(mode === "lightmode"){
+      setMode("darkmode")
+      renderDarkModeText()
+    }else {
+      setMode("lightmode")
+      renderDarkModeText()
+    }
+  }
+
+  function renderDarkModeText(){
+    if(mode === "lightmode"){
+      setText("")
+    }else if (mode === "darkmode"){
+      setText("Tienes activado el dark mode")
+    }
   }
 
   return (
-    <div>
-    <button onClick={handleClick}>Des/activar el dark mode</button>
-    <p>Tienes activado el dark mode</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam unde aliquid voluptate, commodi omnis consequuntur molestias, inventore modi cumque repellendus voluptatum odio minus, et nostrum fugiat! Sint voluptates dolor similique!</p>
+    <div className={mode}>
+      <button onClick={handleClick}>Des/activar el dark mode</button>
+      <p>{text}</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam unde aliquid voluptate, commodi omnis consequuntur molestias, inventore modi cumque repellendus voluptatum odio minus, et nostrum fugiat! Sint voluptates dolor similique!</p>
     </div>
   )
 }
